83b5e46807b502fdf2a7734103ad837cee81be6e
MidnightStudio <Almondshin8@gmail.com>
2022-05-12 11:21:15 +0900

    루틴팀, 마이페이지팀 병합

--- front/navigation/WorkoutsNav.js
+++ front/navigation/WorkoutsNav.js
@@ -9,9 +9,10 @@ import StrengthMain from '../screens/Workouts/StrengthMain';
 import StrengthDetail from '../screens/Workouts/StrengthDetail';
 import Endurance from '../screens/Workouts/Endurance';
 import Mobility from '../screens/Workouts/Mobility';
-import level1 from '../screens/Workouts/level1';
-import level2 from '../screens/Workouts/level2';
-import level3 from '../screens/Workouts/level3';
+import Level1 from '../screens/Workouts/level1';
+import Level2 from '../screens/Workouts/level2';
+import Level3 from '../screens/Workouts/level3';
+import Strength from '../screens/Workouts/Strength';
 
 const Tab = createMaterialTopTabNavigator()
 const Stack = createNativeStackNavigator()
@@ -25,29 +26,32 @@ const Workouts = () => {
         tabBarStyle: { backgroundColor: "grey" }
       }}>
       <Tab.Screen name="ForYouNav" component={ForYouNav} options={{ tabBarLabel: 'For You' }} />
-      <Tab.Screen name="BrowseNav" component={BrowseNav} options={{ tabBarLabel: 'Browse' }} />
+      <Tab.Screen name="Browse" component={Browse} options={{ tabBarLabel: 'Browse' }} />
       <Tab.Screen name="Plans" component={Plans} options={{ tabBarLabel: 'My Plan' }} />
     </Tab.Navigator>
   )
 }
 
-const BrowseNav = () => {
+const BrowseDetail = () => {
   return (
-    <Stack.Navigator initialRouteName="BrowseMain" screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="Browse" component={Browse} />
-      <Stack.Screen name="Strength" component={Strength} />
-      <Stack.Screen name="Endurance" component={Endurance} />
-      <Stack.Screen name="Mobility" component={Mobility} />
-    </Stack.Navigator>
+    <Tab.Navigator
+      initialRouteName="Endurance"
+      screenOptions={{
+        tabBarLabelStyle: { fontSize: 15, color: "black" },
+        tabBarStyle: { backgroundColor: "grey" }
+      }}>
+      <Tab.Screen name="Strength" component={Strength} />
+      <Tab.Screen name="Endurance" component={Endurance} />
+      <Tab.Screen name="Mobility" component={Mobility} />
+    </Tab.Navigator>
   )
 }
 
-const Strength = () => {
+const StrengthNav = () => {
   return (
-    <Stack.Navigator initialRouteName="StrengthMain" screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="StrengthMain" component={StrengthMain} />
-      <Stack.Screen name="StrengthDetail" component={StrengthDetail} />
-    </Stack.Navigator>
+      <Stack.Navigator initialRouteName="StrengthDetail">
+          <Stack.Screen name="StrengthDetail" component={StrengthDetail} />
+      </Stack.Navigator>
   )
 }
 
@@ -63,19 +67,22 @@ const ForYouNav = () => {
 const LevelNav = () => {
   return (
     <Stack.Navigator>
-      <Stack.Screen name='LEVEL1' component={level1} />
-      <Stack.Screen name='LEVEL2' component={level2} />
-      <Stack.Screen name='LEVEL3' component={level3} />
+      <Stack.Screen name='LEVEL1' component={Level1} />
+      <Stack.Screen name='LEVEL2' component={Level2} />
+      <Stack.Screen name='LEVEL3' component={Level3} />
     </Stack.Navigator>
   );
 }
 
 const WorkoutsNav = () => {
   return (
-    <Stack.Navigator>
-      <Stack.Screen name='Workouts' component={Workouts} />
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="Workouts" component={Workouts} />
+      <Stack.Screen name="BrowseDetail" component={BrowseDetail} />
+      <Stack.Screen name="StrengthNav" component={StrengthNav} />
     </Stack.Navigator>
   )
 }
+
 
 export default WorkoutsNav
--- front/screens/Workouts/ForYou.js
+++ front/screens/Workouts/ForYou.js
@@ -4,14 +4,14 @@ import { StyleSheet, Text, View, TouchableOpacity} from 'react-native'
 const ForYou = ({ navigation }) => {
     return (
         <View style={styles.buttonContainer}>
-          <TouchableOpacity onPress={() => { navigation.navigate('levelNav', { screen: 'LEVEL1'}) }} style={styles.button}>
-            <Text>LEVEL1</Text>
+          <TouchableOpacity onPress={() => { navigation.navigate('LevelNav', { screen: 'LEVEL1'}) }} style={styles.button}>
+            <Text>초급</Text>
           </TouchableOpacity>
-          <TouchableOpacity onPress={() => { navigation.navigate('levelNav', { screen: 'LEVEL2'}) }} style={styles.button}>
-            <Text>LEVEL2</Text>
+          <TouchableOpacity onPress={() => { navigation.navigate('LevelNav', { screen: 'LEVEL2'}) }} style={styles.button}>
+            <Text>중급</Text>
           </TouchableOpacity>
-          <TouchableOpacity onPress={() => { navigation.navigate('levelNav', { screen: 'LEVEL3'}) }} style={styles.button}>
-            <Text>LEVEL3</Text>
+          <TouchableOpacity onPress={() => { navigation.navigate('LevelNav', { screen: 'LEVEL3'}) }} style={styles.button}>
+            <Text>고급</Text>
           </TouchableOpacity>
         </View>
       );
