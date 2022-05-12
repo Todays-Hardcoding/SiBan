ad11c7a373d5c5eafe061c4ee7cef9763bbb35d2
Jinho Kim <wlsghrla94@gmail.com>
2022-05-12 15:27:43 +0900

    재병합

--- front/App.js
+++ front/App.js
@@ -1,8 +1,10 @@
 import { NavigationContainer, StackActions } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 import MainTabs from './navigation/MainTabs';
-import MyPageNav from './navigation/MyPageNav'
+import MyPage from './navigation/MyPage'
+import LoginPage from './navigation/LoginPage';
 
 const Stack = createStackNavigator();
 
@@ -12,11 +14,10 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name='MainTabs' component={MainTabs} options={{ headerShown: false }}></Stack.Screen>
-        <Stack.Screen name='MyPageNav' component={MyPageNav} options={{ headerShown: false }}></Stack.Screen>
+        <Stack.Screen name='MyPage' component={MyPage}></Stack.Screen>
+        <Stack.Screen name='LoginPage' component={LoginPage}></Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
 
   );
 }
-
-
--- front/screens/Home.js
+++ front/screens/Home.js
@@ -1,13 +1,119 @@
-import { NativeStackScreenProps } from "@react-navigation/native-stack";
-import React, { useEffect, useState } from "react";
-import { Image, Text, ActivityIndicator, Dimensions, RefreshControl, ScrollView, StyleSheet, useColorScheme, View } from "react-native";
+import React, { useState } from 'react';
 
-const Home = () => {
+import { Text, View, TextInput, StyleSheet, TouchableOpacity } from 'react-native';
+//import TextInput from 'react-native-input-validator';
+//import KakaoButton from '../../components/KakaoButton';
+//import LoginButton from '../../components/LoginButton';
+import KakaoButton from '../components/KakaoButton';
+import LoginButton from '../components/LoginButton';
+import { NavigationContainer } from '@react-navigation/native';
+//import { State } from 'react-native-gesture-handler';
+
+
+const Login = ({ navigation }) => {
+  
+  const [loginId, setLoginId] = useState("");
+ // const [loginPw, setLoginPw] = useState("");
+
+  const [loginIdCheck, setLoginIdCheck] = useState(false);
+  //const [loginPwCheck, setLoginPwCheck] = useState(false);
+
+  const [loginError, setLoginError] = useState("");
+  
+
+  
+  console.log(loginId)
+
+  const onChangeId = (text) => {
+
+    if (text.trim().length === 0) {
+      setLoginIdCheck(false)
+    } else {
+      setLoginIdCheck(true);
+    }
+    setLoginId(text);
+    console.log(loginIdCheck);
+    console.log(loginId);
+
+
+  }
+
+
+
+
   return (
-    <View>
-      <Text>Home 화면</Text>
-    </View>
-  )
-}
 
-export default Home;
+    <View
+      style={{
+        flex: 0.75,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <Text style={{ fontSize: 20, fontWeight: "bold" }}>시반로그인</Text>
+      <Text> </Text>
+
+      {/* 아이디 입력하는 곳 */}
+      <TextInput
+        style={styles.input}
+        placeholder={"아이디"}
+        //label='Email'
+        value={loginId}
+        //onChangeText={(email) => setEmail(email)}
+        //error={hasEmailErrors}
+        onChangeText={(text) => onChangeId(text)}
+      />
+      <View>
+        {loginIdCheck == false ? <Text style={{color:"red"}}>아이디를 입력해주세요</Text> : null}
+      </View>
+
+
+
+      <TextInput
+        style={styles.input}
+        placeholder={"비밀번호"}
+        secureTextEntry={true}
+      />
+
+
+      <TouchableOpacity style={{ flexDirection: "row" }}>
+        <Text style={{ color: "gray" }} onPress={() => navigation.navigate('LoginPage', {
+          screen: 'Search'
+        })}>아이디/비번찾기       </Text>
+        <Text style={{ color: "gray" }} onPress={() => navigation.navigate('LoginPage', {
+          screen: 'Register'
+        })}>        회원가입</Text>
+
+
+      </TouchableOpacity>
+
+      <LoginButton />
+      <KakaoButton />
+
+
+    </View>
+
+
+
+  );
+};
+
+
+const styles = StyleSheet.create({
+  input: {
+    borderWidth: 1,
+    padding: 10,
+    fontSize: 20,
+    borderRadius: 10,
+    paddingVertical: 10,
+
+    marginVertical: 10,
+    paddingHorizontal: 32,
+    marginBottom: 16,
+    width: '85%'
+
+  }
+});
+
+export default Login;
