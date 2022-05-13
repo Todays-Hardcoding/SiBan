7dd024f6c0427838497bc0eba5bcd5cbd85140a7
Almondshin <Almondshin8@gmail.com>
2022-05-13 16:41:13 +0900

    레포레포

--- back/src/main/java/com/siban/back/mypage/repository/DietRepository.java
+++ back/src/main/java/com/siban/back/mypage/repository/DietRepository.java
@@ -1,10 +1,11 @@
 package com.siban.back.mypage.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.siban.back.mypage.domain.Diet;
 
-
+@Repository
 public interface DietRepository extends JpaRepository<Diet, Long> {
 
 }
--- back/src/main/java/com/siban/back/mypage/repository/ScheduleRepository.java
+++ back/src/main/java/com/siban/back/mypage/repository/ScheduleRepository.java
@@ -1,9 +1,11 @@
 package com.siban.back.mypage.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.siban.back.mypage.domain.Schedule;
 
+@Repository
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
 
 }
