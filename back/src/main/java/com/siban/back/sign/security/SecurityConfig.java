e466cc96ccf69be898806bc2d1edba33946433c6
pipi <pkh9847@gmail.com>
2022-05-16 13:32:17 +0900

    Merge branch 'pipi' of https://github.com/Todays-Hardcoding/SiBan.git into pipi

--- back/src/main/java/com/siban/back/sign/security/SecurityConfig.java
+++ back/src/main/java/com/siban/back/sign/security/SecurityConfig.java
@@ -1,5 +1,25 @@
 package com.siban.back.sign.security;
 
-public class SecurityConfig {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		// TODO Auto-generated method stub
+		http.authorizeHttpRequests()
+		.antMatchers("/")
+		.access("hasRole('ROLE_USER')")
+		
+		
+	}
+	
+	
 
 }
