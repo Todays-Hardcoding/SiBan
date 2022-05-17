c1151c38e311dd85396e3b28dad73483882670ab
pipi <pkh9847@gmail.com>
2022-05-17 10:07:35 +0900

    일단

--- back/src/main/java/com/siban/back/sign/controller/SignController.java
+++ back/src/main/java/com/siban/back/sign/controller/SignController.java
@@ -5,15 +5,20 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.siban.back.sign.domain.User;
 import com.siban.back.sign.service.SignService;
+
+import lombok.Builder;
 
 
 @RestController
@@ -22,8 +27,10 @@ public class SignController {
 	@Autowired
 	private SignService signService;
 	
+	
 	@RequestMapping(value="/register.act", method = RequestMethod.POST)
 	public User register(@RequestBody Map<String, Object> param) {
+		//User user = User.builder().userId(param.get("id")).build();
 		User user = new User();
 		String id = (String) param.get("id");
 		String pw = (String) param.get("pw");
@@ -41,11 +48,18 @@ public class SignController {
 		user.setUserHeight(height);
 		user.setUserWeight(weight);
 		
+		//boolean result = false;
+		//if(signService.findbyUserId(id).isPresent()) {
+			
+	
 		signService.insertUser(user);
 		
+	
+		
 		return user;
+	
 	}
-
+	
 	
 	
 
--- back/src/main/java/com/siban/back/sign/service/SignService.java
+++ back/src/main/java/com/siban/back/sign/service/SignService.java
@@ -1,6 +1,8 @@
 package com.siban.back.sign.service;
 
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,5 +17,11 @@ public class SignService{
 	public User insertUser(User user) {
 		return signRepository.save(user);
 	}
+	
+	public Optional<User> findbyUserId(String userId) {
+		return signRepository.findByUserId(userId);
+	}
+
+
 
 }
