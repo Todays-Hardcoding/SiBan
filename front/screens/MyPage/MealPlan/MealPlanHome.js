9df45b3e3107a2f03b047892d44eac47ce6bb99e
Jinho Kim <wlsghrla94@gmail.com>
2022-05-18 00:58:04 +0900

    식단일기 최종

--- front/screens/MyPage/MealPlan/MealPlanHome.js
+++ front/screens/MyPage/MealPlan/MealPlanHome.js
@@ -13,7 +13,6 @@ const View = styled.View`
 const MainText = styled.Text`
   color: white;
   font-size: 38px;
-  // margin-bottom: 70px;
   font-weight: 900;
   margin-top: 20px;
 `;
@@ -22,27 +21,44 @@ const Title = styled.Text`
   color: white;
   margin-top: 10px;
   margin-right: 40px;
-  margin-bottom: 20px
+  margin-bottom: 10px
   text-align: right;
   font-size: 20px;
   font-weight: 500;
 `;
-const TextInput = styled.TextInput`
+const MealTimeInput = styled.TextInput`
+  background-color: white;
+  border-radius: 10px;
+  padding-vertical: 10px;
+  padding-horizontal: 15px;
+  font-size: 15px;
+  width: 30%;
+`;
+const MealInput = styled.TextInput`
   background-color: white;
   border-radius: 20px;
   padding-vertical: 10px;
   padding-horizontal: 30px;
   font-size: 15px;
-  margin-bottom: 20px;
+  margin-bottom: 10px;
+`;
+const CommentInput = styled.TextInput`
+  background-color: white;
+  border-radius: 20px;
+  padding-vertical: 10px;
+  padding-horizontal: 30px;
+  font-size: 15px;
+  margin-bottom: 10px;
 `;
 const Btn = styled.TouchableOpacity`
   width: 100%;
-  margin-top: 30px;
-  margin-bottom: 30px;
+  margin-top: 5px;
+  margin-bottom: 20px;
   background-color: #B5DEFF;
   padding: 10px 20px
   align-items: center;
   border-radius: 20px;
+  
 `;
 const BtnText = styled.Text`
   color: white;
@@ -57,7 +73,7 @@ const GradeView = styled.View`
 const Grade = styled.TouchableOpacity`
   background-color: white;
   elevation: 5;
-  padding: 10px;
+  padding: 10px 23px;
   border-radius: 15px;
   overflow: hidden;
   border-width: ${(props) => (props.selected ? "2px" : "0px")};
@@ -74,7 +90,7 @@ const TimeView = styled.View`
 const Time = styled.TouchableOpacity`
   background-color: white;
   elevation: 5;
-  padding: 10px;
+  padding: 10px 25px;
   border-radius: 15px;
   overflow: hidden;
   border-width: ${(props) => (props.selected ? "2px" : "0px")};
@@ -84,26 +100,58 @@ const TimeText = styled.Text`
   font-size: 18px;
 `;
 const MyMealScrollView = styled.ScrollView``;
-const MyMealView = styled.View`
-  background-color: white;
+const MyMealDietView = styled.View`
+  background-color: #63cdda;
   margin-bottom: 10px;
   padding-vertical: 15px;
   padding-horizontal: 20px;
   border-radius: 15;
 `;
-const MyMealText = styled.Text``;
+const MyMealCheatView = styled.View`
+  background-color: #cf6a87;
+  margin-bottom: 10px;
+  padding-vertical: 15px;
+  padding-horizontal: 20px;
+  border-radius: 15;
+`;
+const MyMealText = styled.Text`
+  font-size: 18px;
+  color: white;
+  font-weight: 500;
+`;
+const MyMealFood = styled.Text`
+  font-size: 27px;
+  color: white;
+  font-weight: 500;
+  margin-bottom: 10px;
+`;
+const MyMealComment = styled.Text`
+  font-size: 25px;
+  color: white;
+  font-weight: 500;
+  margin-bottom: 10px;
+`;
 const MyMealDelete = styled.Text``;
-const MyMealDeleteTouchableOpacity = styled.TouchableOpacity``;
+const MyMealDeleteTouchableOpacity = styled.TouchableOpacity`
+  align-items: flex-end;
+`;
+const MyMealWhenGradeTime = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  margin-bottom: 10px;
+`;
 
 const STORAGE_KEY = "@mealPlan";
 
-const grades = ["😆 diet meal", "😡 cheat meal"];
+const grades = ["😆 diet", "😡 cheat"];
 const times = ["🥙 아침", "🍱 점심", "🥘 저녁"];
 
 const MealPlanWrite = ({ navigation: { goBack } }) => {
   const [selectedGrade, setGrade] = useState(null);
   const [selectedTime, setTime] = useState(null);
   const [foods, setFoods] = useState("");
+  const [comment, setComment] = useState("");
+  const [when, setWhen] = useState("");
   const [mealPlan, setMealPlan] = useState({});
 
   useEffect(() => {
@@ -114,6 +162,9 @@ const MealPlanWrite = ({ navigation: { goBack } }) => {
   const onTimePress = (time) => setTime(time);
 
   const onChangeFood = (payload) => setFoods(payload);
+  const onChangeComment = (payload) => setComment(payload);
+  const onChangeWhen = (payload) => setWhen(payload);
+
   const saveMealPlan = async (toSave) => {
     await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(toSave));
   };
@@ -135,11 +186,21 @@ const MealPlanWrite = ({ navigation: { goBack } }) => {
 
     const newMealPlan = {
       ...mealPlan,
-      [Date.now()]: { foods, grade: selectedGrade, time: selectedTime },
+      [Date.now()]: {
+        when,
+        selectedGrade,
+        selectedTime,
+        foods,
+        comment,
+      },
     };
     setMealPlan(newMealPlan);
     await saveMealPlan(newMealPlan);
     setFoods("");
+    setGrade(null);
+    setTime(null);
+    setComment("");
+    setWhen("");
   };
   const deleteTodo = (key) => {
     Alert.alert("나의 식단 지우기", "삭제하시겠습니까?", [
@@ -169,6 +230,41 @@ const MealPlanWrite = ({ navigation: { goBack } }) => {
     >
       <MainText>나의 식단 기록일기</MainText>
       <Title>오늘 어떤 음식을 드셨나요?</Title>
+      <MyMealScrollView>
+        {Object.keys(mealPlan).map((key) =>
+          mealPlan[key].selectedGrade === "😆 diet" ? (
+            <MyMealDietView key={key}>
+              <MyMealWhenGradeTime>
+                <MyMealText>날짜: {mealPlan[key].when}</MyMealText>
+                <MyMealText>{mealPlan[key].selectedGrade}</MyMealText>
+                <MyMealText>{mealPlan[key].selectedTime}</MyMealText>
+              </MyMealWhenGradeTime>
+              <MyMealWhenGradeTime>
+                <MyMealFood>{mealPlan[key].foods}</MyMealFood>
+                <MyMealDeleteTouchableOpacity onPress={() => deleteTodo(key)}>
+                  <MyMealDelete>❌</MyMealDelete>
+                </MyMealDeleteTouchableOpacity>
+              </MyMealWhenGradeTime>
+              <MyMealComment>{mealPlan[key].comment}</MyMealComment>
+            </MyMealDietView>
+          ) : (
+            <MyMealCheatView key={key}>
+              <MyMealWhenGradeTime>
+                <MyMealText>날짜: {mealPlan[key].when}</MyMealText>
+                <MyMealText>{mealPlan[key].selectedGrade}</MyMealText>
+                <MyMealText>{mealPlan[key].selectedTime}</MyMealText>
+              </MyMealWhenGradeTime>
+              <MyMealWhenGradeTime>
+                <MyMealFood>{mealPlan[key].foods}</MyMealFood>
+                <MyMealDeleteTouchableOpacity onPress={() => deleteTodo(key)}>
+                  <MyMealDelete>❌</MyMealDelete>
+                </MyMealDeleteTouchableOpacity>
+              </MyMealWhenGradeTime>
+              <MyMealComment>{mealPlan[key].comment}</MyMealComment>
+            </MyMealCheatView>
+          )
+        )}
+      </MyMealScrollView>
       <GradeView>
         {grades.map((grade, index) => (
           <Grade
@@ -179,6 +275,14 @@ const MealPlanWrite = ({ navigation: { goBack } }) => {
             <GradeText>{grade}</GradeText>
           </Grade>
         ))}
+        <MealTimeInput
+          returnKeyLabel="done"
+          // 전송버튼을 누를때
+          onSubmitEditing={addMealPlan}
+          onChangeText={onChangeWhen}
+          value={when}
+          placeholder="날짜"
+        ></MealTimeInput>
       </GradeView>
       <TimeView>
         {times.map((time, index) => (
@@ -192,28 +296,26 @@ const MealPlanWrite = ({ navigation: { goBack } }) => {
         ))}
       </TimeView>
 
-      <TextInput
+      <MealInput
         returnKeyLabel="done"
         // 전송버튼을 누를때
         onSubmitEditing={addMealPlan}
         onChangeText={onChangeFood}
         value={foods}
         placeholder="메뉴를 적으세요."
-      ></TextInput>
+      ></MealInput>
+      <CommentInput
+        returnKeyLabel="done"
+        // 전송버튼을 누를때
+        onSubmitEditing={addMealPlan}
+        onChangeText={onChangeComment}
+        value={comment}
+        placeholder="한 줄 평가."
+      ></CommentInput>
 
       <Btn onPress={addMealPlan}>
         <BtnText>Save</BtnText>
       </Btn>
-      <MyMealScrollView>
-        {Object.keys(mealPlan).map((key) => (
-          <MyMealView key={key}>
-            <MyMealText>{mealPlan[key].foods}</MyMealText>
-            <MyMealDeleteTouchableOpacity onPress={() => deleteTodo(key)}>
-              <MyMealDelete>❌</MyMealDelete>
-            </MyMealDeleteTouchableOpacity>
-          </MyMealView>
-        ))}
-      </MyMealScrollView>
     </View>
   );
 };
