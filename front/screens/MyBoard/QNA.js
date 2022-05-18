f5101cec08eb5650cb28c997e5a349024668e826
okaymina <okaymina418@gmail.com>
2022-05-18 15:18:05 +0900

    글작성 버튼 색

--- front/screens/MyBoard/QNA.js
+++ front/screens/MyBoard/QNA.js
@@ -122,7 +122,7 @@ const styles = StyleSheet.create({
   submit: {
     width: 120,
     height: 40,
-    backgroundColor: "#EEB0B0",
+    backgroundColor: "#F2C9E1",
     alignItems: "center",
     justifyContent: "center",
     margin: 15,
@@ -131,7 +131,7 @@ const styles = StyleSheet.create({
   cancel: {
     width: 120,
     height: 40,
-    backgroundColor: "#AACFCF",
+    backgroundColor: "#B1BCE6",
     alignItems: "center",
     justifyContent: "center",
     margin: 15,
