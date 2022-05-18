26168ff1a66a6c32b55225650294bc65042c570d
Jinho Kim <wlsghrla94@gmail.com>
2022-05-18 23:45:22 +0900

    .

--- front/screens/Workouts/DetailPage.js
+++ front/screens/Workouts/DetailPage.js
@@ -2,21 +2,42 @@ import React from "react";
 import { FontAwesome } from "@expo/vector-icons";
 import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
 
-const DetailPage = ({ route, navigation }) => {
+const DetailPage = ({ route }) => {
   const { exercise } = route.params;
-
+  // 콘솔 안나옴
+  console.log(exercise.workoutName);
   return (
     <View style={styles.Container}>
-      <View style={styles.headerContainer}>
-        <TouchableOpacity onPress={() => navigation.pop()}>
-          <FontAwesome name="arrow-left" size={24} color="black" />
-        </TouchableOpacity>
-        <TouchableOpacity>
-          <FontAwesome name="bookmark" size={24} color="black" />
-        </TouchableOpacity>
-      </View>
-      <View>
-        <Text>{exercise.workoutName}</Text>
+      <View style={styles.secondContainer}>
+        <View style={styles.headerContainer}>
+          <TouchableOpacity>
+            <FontAwesome
+              style={styles.headerBtn}
+              name="arrow-left"
+              size={24}
+              color="black"
+            />
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <FontAwesome
+              style={styles.headerBtn}
+              name="bookmark"
+              size={24}
+              color="black"
+            />
+          </TouchableOpacity>
+        </View>
+        <View style={styles.workoutNameView}>
+          <Text style={styles.workoutNameText}>{exercise.workoutName}</Text>
+        </View>
+        <View style={styles.workoutDescriptionView}>
+          <Text style={styles.workoutDescriptionText}>
+            {exercise.workoutDescription}
+          </Text>
+        </View>
+        <Text>{exercise.workoutCourse}</Text>
+        <Text>{exercise.workoutGoal}</Text>
+        <Text>{exercise.workoutTarget}</Text>
       </View>
     </View>
   );
@@ -25,12 +46,33 @@ const DetailPage = ({ route, navigation }) => {
 const styles = StyleSheet.create({
   Container: {
     flex: 1,
+    backgroundColor: "#252525",
+    alignItems: "center",
   },
   headerContainer: {
     flexDirection: "row",
     justifyContent: "space-between",
-    marginHorizontal: 20,
-    marginTop: 20,
+  },
+  secondContainer: { width: "90%", flex: 1 },
+  headerBtn: { color: "white" },
+  workoutNameView: {
+    flex: 1,
+    verticalAlign: "bottom",
+    backgroundColor: "red",
+  },
+  workoutNameText: {
+    fontSize: "50",
+    fontWeight: "500",
+    color: "white",
+  },
+  workoutDescriptionView: {
+    flex: 1,
+    backgroundColor: "#252525",
+  },
+  workoutDescriptionText: {
+    color: "white",
+    fontSize: "20",
+    fontWeight: "500",
   },
 });
 
