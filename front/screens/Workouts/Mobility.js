63c3ca5a994e55c782e261a2acd6930e03706557
woo <dndlsgur77@gmail.com>
2022-05-19 13:54:43 +0900

    플랜 수정

--- front/screens/Workouts/Mobility.js
+++ front/screens/Workouts/Mobility.js
@@ -20,7 +20,8 @@ const Mobility = ({ navigation }) => {
       .then((response) => response.json())
       .then((data) => {
         setExercises(data);
-      });
+      })
+      .catch((e) => console.log(e))
   }, []);
 
   return (
