1bd0bb2e3bfb342558fd3594473e9f70ddaae7d3
HazZlng <63578131+HazZlng@users.noreply.github.com>
2022-05-20 11:05:20 +0900

    .

--- back/src/main/java/com/siban/back/board/domain/Post.java
+++ back/src/main/java/com/siban/back/board/domain/Post.java
@@ -9,11 +9,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.siban.back.sign.domain.User;
 
 import lombok.Builder;
@@ -27,7 +30,11 @@ import lombok.Setter;
 @Setter
 @Entity
 public class Post {
-//	private User user;
+	@NotNull
+	@ManyToOne
+	@JsonBackReference
+	@JoinColumn(name="userId")
+	private User user;
 	
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
--- back/src/main/java/com/siban/back/board/domain/Reply.java
+++ back/src/main/java/com/siban/back/board/domain/Reply.java
@@ -7,8 +7,11 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotNull;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.siban.back.sign.domain.User;
 
 import lombok.Data;
@@ -16,8 +19,17 @@ import lombok.Data;
 @Data
 @Entity
 public class Reply {
-//	private User user;
-//	private Post post;
+	@NotNull
+	@ManyToOne
+	@JsonBackReference
+	@JoinColumn(name="userId")
+	private User user;
+	
+	@NotNull
+	@ManyToOne
+	@JsonBackReference
+	@JoinColumn(name="postCode")
+	private Post post;
 
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
